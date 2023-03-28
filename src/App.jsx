6dab89a0b019c56f68b3c3e5237cd6521b9322e4
Paul Voronin <voron.paul@gmail.com>
2023-03-28 10:33:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import React, { useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei'
+import Box from './components/Box';
+
+const App = () => {
+  const group = useRef();
+
+  return (
+    <>
+      <h1>Storage box for everyday</h1>
+      <p>Use your mouse to drag, rotate, zoom the box. Click on the buttons to open the clasters. Just try it! </p>
+
+      <div className="canvas">
+        <Canvas camera={{ position: [-1, 18, 5] }}>
+          <OrbitControls />
+          <directionalLight position={[5, 12, 5]} intensity={1} />
+          <ambientLight intensity={0.2} />
+          <group ref={group}>
+            <Box />
+          </group>
+        </Canvas>
+      </div>
+
+      <p>Developer Pavlo Voronin, 2023 <b>|</b> Especially for Happyusers as a test task </p>
+    </>
+  );
+};
+
+export default App;
