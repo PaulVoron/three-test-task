d5651ddc4f4eef31893ea0442560808efe825c33
Paul Voronin <voron.paul@gmail.com>
2023-03-28 12:48:25 +0300

    add assets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/three-test-task'
+  base: '/three-test-task/'
 })
